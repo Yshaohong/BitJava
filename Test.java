64d1ea4b398ebe462cf1d25dc713c326e07ffd05
Yshaohong <2356657597@qq.com>
2019-11-15 22:39:43 +0800

    java交换两个数以及最大值最小值

---
+++ Test.java
@@ -0,0 +1,46 @@
+/*public class Test
+{
+    public static void main(String[] args)
+    {
+
+        int i=10;
+        int j=20;
+        i=i^j;
+        j=i^j;
+        i=i^j;
+        System.out.printf("i=%d,j=%d",i,j);
+        
+    }
+}*/
+
+/*public class Test
+{
+    public static void main(String[] args)
+    {
+        int a=30;
+        int b=20;
+        int c=40;
+        int i=0;
+        if(a<b)
+        {
+            i=b;
+            b=a;
+            a=i;
+        }
+        if(a<c)
+        {
+            i=c;
+            c=a;
+            a=i;
+        }
+        if(b<c)
+        {
+            i=b;
+            b=c;
+            c=i;
+        }
+        System.out.println(a);
+        System.out.println(c);
+    }
+}*/
+
